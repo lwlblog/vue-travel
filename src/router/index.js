bda14875803283f054f41480c7b2a0ed533b5fb2
liwenlong <1519523354@qq.com>
2020-02-18 15:40:03 +0800

    finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -10,15 +10,15 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('@/pages/home/Home')
   }, {
     path: '/city',
     name: 'City',
-    component: City
+    component: () => import('@/pages/city/City')
   }, {
     path: '/detail/:id',
     name: 'Detail',
-    component: Detail
+    component: () => import('@/pages/detail/Detail')
   }],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
